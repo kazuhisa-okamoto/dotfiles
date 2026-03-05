99304a862323fc9297b1b2041f3fbaf3f27c3166
Kazuhisa Okamoto <okamoto1834@gmail.com>
2026-03-05 09:20:55 +0900

    exclude user_path.ps1

--- powershell/user_path.ps1
+++
@@ -1,8 +0,0 @@
-$notedir = "$env:USERPROFILE\Dropbox\Documents\note"
-
-$mydirs = @(
-    $notedir
-    "$env:USERPROFILE\Dropbox\code"
-    "$env:USERPROFILE\Dropbox\Documents"
-    "$env:USERPROFILE\src"
-)
