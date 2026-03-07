de69b22e1124058fc4d942290f83169629a7f470
Kazuhisa Okamoto <okamoto1834@gmail.com>
2026-03-07 09:28:15 +0900

    feat(nvim): add completion setup

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,14 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    vim.lsp.config("pyright", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable("pyright")
+
+  end,
+}
